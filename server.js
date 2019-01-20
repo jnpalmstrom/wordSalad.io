0762c03230ad3e589269a9f8a8464d4b1f56ea6a
jnpalmstrom <jnpalmstrom@gmail.com>
2019-01-19 23:13:33 -0500

    Added word functionality

--- server.js
+++ server.js
@@ -3,22 +3,541 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 const rword = require('rword');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const CollegiateDictionary = require("mw-dict").CollegiateDictionary;
+let adjList = [];
+let nounList = [];
+let verbList = [];
 
-const MW_API_KEY = 'f79df243-30e7-417b-9072-5161ebc7e154';
-const dict = new CollegiateDictionary(MW_API_KEY);
+//const starterWords = ['furniture', 'greeting', 'drink', 'fuck', 'drugs', 'whores', 'food', 'school', 'tools', 'mood'];
+request('https://api.datamuse.com/words?md=p&sp=a*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+
+request('https://api.datamuse.com/words?md=p&sp=b*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=c*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=d*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=e*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=f*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=g*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=h*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=i*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=j*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=k*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=l*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=m*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=n*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=o*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=p*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=q*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=r*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=s*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=t*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=u*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=v*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=w*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=x*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=y*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
+request('https://api.datamuse.com/words?md=p&sp=z*', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    body.forEach((item) => {
+        if (typeof item.tags !== 'undefined') {
+            switch (item.tags[0]) {
+                case 'adj':
+                    adjList.push(item);
+                    break;
+                case 'n':
+                    nounList.push(item);
+                    break;
+                case 'v':
+                    verbList.push(item);
+                    break;
+                default:
+                    break;
+            }
+        }
+    });
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, '/palavramix/build')));
-
-// Sets relative path for Express to serve files out of views folder
-//app.use(express.static(__dirname + '/public'));
-//app.use(express.static(__dirname + '/views'));
 
 // For Socket.io
 const server = require('http').createServer(app);
@@ -35,10 +554,9 @@ let allPosts = [];
 let currColor = 1;
 
 let allWords = [];
-allWords = rword.rword.generate(8);
-allWords.push('The');
-allWords.push('A/An');
-console.log(allWords);
+allWords = generateWords();
+allWords.push('the');
+allWords.push('a/an');
 
 let postID = 0;
 
@@ -116,9 +634,6 @@ io.on('connection', function (socket) {
 
 });
 
-const allFunctionalLabels = ['adjective', 'noun', 'adverb', 'preposition', 'conjunction',
-    'pronoun', 'interjection', 'verb'];
-
 // Randomly chooses 5 functional labels
 function labelPicker() {
     let newSetOfLabels = ['adjective', 'noun', 'verb'];
@@ -130,13 +645,29 @@ function labelPicker() {
     return newSetOfLabels;
 }
 
-//
-function generateWords(setOfLabels) {
+// Generate 8 random words
+function generateWords() {
+    let newWords = [];
 
-    setOfLabels.forEach((aLabel) => {
-        let randomWord = rword.generate();
-    });
+    // 3 Nouns, 2 Verbs, 3 Adj
+    var rand1 = nounList[Math.floor(Math.random() * nounList.length)];
+    var rand2 = nounList[Math.floor(Math.random() * nounList.length)];
+    var rand3 = nounList[Math.floor(Math.random() * nounList.length)];
+    var rand4 = verbList[Math.floor(Math.random() * verbList.length)];
+    var rand5 = verbList[Math.floor(Math.random() * verbList.length)];
+    var rand6 = adjList[Math.floor(Math.random() * adjList.length)];
+    var rand7 = adjList[Math.floor(Math.random() * adjList.length)];
+    var rand8 = adjList[Math.floor(Math.random() * adjList.length)];
 
+    newWords.push(rand1);
+    newWords.push(rand2);
+    newWords.push(rand3);
+    newWords.push(rand4);
+    newWords.push(rand5);
+    newWords.push(rand6);
+    newWords.push(rand7);
+    newWords.push(rand8);
+    return newWords;
 }
 
 // Loop thru all connected sockets and notify that Posts will be deleted soon
@@ -181,7 +712,7 @@ function clearPosts() {
 
     // Clear all words
     allWords = [];
-    allWords = rword.rword.generate(8);
+    allWords = generateWords();
     allWords.push('The');
     allWords.push('A/An');
     shuffle(allWords);
